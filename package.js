db3754a697720e5bf3f7c354405550c6b28f049b
Pierre Wieser <pwieser@trychlos.org>
2023-06-20 23:50:21 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:layout',
-    version: '1.2.2',
+    version: '1.2.3-rc',
     summary: 'Client-only Meteor package for responsive layout management',
     git: 'https://github.com/trychlos/pwix:layout',
     documentation: 'README.md'
