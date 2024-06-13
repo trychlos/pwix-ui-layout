638cf67fc9be2355cc077f0dd85abcd67e3ac48b
Pierre Wieser <p.wieser@trychlos.org>
2024-06-13 11:53:17 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:ui-layout',
-    version: '2.0.1',
+    version: '2.0.2-rc',
     summary: 'Meteor package for responsive layout management',
     git: 'https://github.com/trychlos/pwix:ui-layout',
     documentation: 'README.md'
